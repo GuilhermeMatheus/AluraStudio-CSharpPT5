17dcf0bfe6e28b2fb134886a6d92aaca1aa53646
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-06 17:23:48 -0300

    Video 2.2 (salvei csproj)

--- ByteBank/ByteBank.Modelos/Funcionarios/Funcionario.cs
+++ ByteBank/ByteBank.Modelos/Funcionarios/Funcionario.cs
@@ -28,6 +28,6 @@ namespace ByteBank.Modelos.Funcionarios
 
         public abstract void AumentarSalario();
 
-        internal abstract double GetBonificacao();
+        internal protected abstract double GetBonificacao();
     }
 }

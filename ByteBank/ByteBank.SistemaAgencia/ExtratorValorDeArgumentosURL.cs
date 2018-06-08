5eb1bb72bf8d48525ec6852c16b3a7255311162b
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-08 17:34:18 -0300

    Video 4.5

--- ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -27,8 +27,18 @@ namespace ByteBank.SistemaAgencia
         // moedaOrigem=real&moedaDestino=dolar
         public string GetValor(string nomeParametro)
         {
-            return "";
-            // int indiceParametro = _argumentos.IndexOf
+            string termo = nomeParametro + "="; // moedaDestino=
+            int indiceTermo = _argumentos.IndexOf(termo); // x
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length); // dolar
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if(indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
         }
 
     }
